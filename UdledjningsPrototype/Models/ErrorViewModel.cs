0cb3795371ac5ea66ed9139672103aa416d4ade6
Nichlas <nichlaschristensen@live.dk>
2020-10-07 09:46:17 +0200

    Add project files.

---
+++ UdledjningsPrototype/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdledjningsPrototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
